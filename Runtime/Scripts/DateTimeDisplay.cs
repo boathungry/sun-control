f0ccdaea446fd67da877170e51333c5b84967ea8
Þór Breki <thorthorgrims@gmail.com>
2026-03-14 19:20:24 +0000

    Making DateTimeDisplay work with TextMeshPro

--- Runtime/Scripts/DateTimeDisplay.cs
+++ Runtime/Scripts/DateTimeDisplay.cs
@@ -1,12 +1,15 @@
 using SunControl;
 using System;
 using UnityEngine;
+using TMPro;
 
 public class DateTimeDisplay : MonoBehaviour
 {
     [SerializeField] private Color textColor = Color.white;
     [SerializeField] private int fontSize = 30;
     public SunControl.SunControl sunControl;
+    public TextMeshProUGUI dateDisplay;
+    public TextMeshProUGUI timeDisplay;
     private DateTime dateTime;
 
     void Start()
@@ -21,21 +24,29 @@ public class DateTimeDisplay : MonoBehaviour
 
     private void OnGUI()
     {
-        int w = Screen.width, h = Screen.height;
-
-        GUIStyle style = new GUIStyle();
-
-        Rect timeRect = new Rect(5, h - 80, w, h);
-        Rect dateRect = new Rect(5, h - 50, w, h);
-
-        style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = fontSize;
-        style.normal.textColor = textColor;
-        
         string timeText = (dateTime != null) ? $"{dateTime.ToShortTimeString()}" : "--:--";
-        string dateText = (dateTime != null) ? $"{dateTime.ToShortDateString()}" : "--:--";
+        string dateText = (dateTime != null) ? $"{dateTime.ToShortDateString()}" : "--/--/----";
 
-        GUI.Label(timeRect, timeText, style);
-        GUI.Label(dateRect, dateText, style);
+        if (dateDisplay != null && timeDisplay != null)
+        {
+            dateDisplay.SetText(dateText);
+            timeDisplay.SetText(timeText);
+        }
+        else
+        {
+            int w = Screen.width, h = Screen.height;
+
+            GUIStyle style = new GUIStyle();
+
+            Rect timeRect = new Rect(5, h - 80, w, h);
+            Rect dateRect = new Rect(5, h - 50, w, h);
+
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = fontSize;
+            style.normal.textColor = textColor;
+
+            GUI.Label(timeRect, timeText, style);
+            GUI.Label(dateRect, dateText, style);
+        }
     }
 }
